a37f6333abe3337cb8c48361e92184efb9e278c2
crownvalz <132905155+crownvalz@users.noreply.github.com>
2023-12-03 22:21:33 +0300

    jh
    h

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: Shuffle
-* Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/bootstrap-3-one-page-template-free-shuffle/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
