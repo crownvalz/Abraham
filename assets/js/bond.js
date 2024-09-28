2f7a6f6b79082fe1648bb197f9c986b122f7bd0c
crownvalz <132905155+crownvalz@users.noreply.github.com>
2024-09-28 18:20:05 +0300

    Update bond.js

---
+++ assets/js/bond.js
@@ -0,0 +1,5 @@
+// Toggle animation for hamburger icon
+const toggler = document.querySelector('.navbar-toggler');
+toggler.addEventListener('click', () => {
+    toggler.classList.toggle('collapsed');
+});
